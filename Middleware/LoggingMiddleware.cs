e14dbb6f2f9e91104a1fdd086d96c0926a5cbc2f
Monisha0685 <155656955+Monisha0685@users.noreply.github.com>
2026-04-24 18:36:34 +0530

    Add LoggingMiddleware for request/response logging

---
+++ Middleware/LoggingMiddleware.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace UserManagementAPI.Middleware
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path}");
+
+            await _next(context);
+
+            Console.WriteLine($"Response: {context.Response.StatusCode}");
+        }
+    }
+
+    public static class LoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLoggingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LoggingMiddleware>();
+        }
+    }
+}
