7a1da3ea3d62fc7b56f2ea1413b564c0dbe53478
Monisha0685 <155656955+Monisha0685@users.noreply.github.com>
2026-04-24 18:29:18 +0530

    Implement UsersController for user management

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using UserManagementAPI.Models;
+
+namespace UserManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _repository;
+
+        public UsersController(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_repository.GetAll());
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var user = _repository.GetById(id);
+            return user == null ? NotFound() : Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            _repository.Add(user);
+            return CreatedAtAction(nameof(GetById), new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, User user)
+        {
+            if (_repository.GetById(id) == null) return NotFound();
+            user.Id = id;
+            _repository.Update(user);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (_repository.GetById(id) == null) return NotFound();
+            _repository.Delete(id);
+            return NoContent();
+        }
+    }
+}
